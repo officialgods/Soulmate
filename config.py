4a2e3048ad24fa73a337155a4bc52984409c9d82
PSYCHO_XD <154287683+officialgods@users.noreply.github.com>
2025-01-27 22:31:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,10 +19,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 180))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID", "1002365374424"))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "6934153226"))
+OWNER_ID = int(getenv("OWNER_ID", "7462077192"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -38,8 +38,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("OUR_NETWORK", ""https://t.me/soulmate_network)
-SUPPORT_CHAT = getenv("GROUP_CHAT", "https://t.me/the_soulmates_club")
+SUPPORT_CHANNEL = getenv("OUR_NETWORK", "https://t.me/+dtFI4SrfNO5iMTNl")
+SUPPORT_CHAT = getenv("GROUP_CHAT", "https://t.me/THE_SOULMATES_HUB")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -77,14 +77,14 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/222f5d905aaf91bda95a1.jpg"
+    "START_IMG_URL", "https://i.imghippo.com/files/mFmE6428fx.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/74731da8fbd13d59b9b90.jpg"
  )
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://graph.org/file/2fd73ef39c62047594009.jpg"
+STATS_IMG_URL = "https://i.imghippo.com/files/mFmE6428fx.jpg"
 TELEGRAM_AUDIO_URL = "https://graph.org/file/6db32c63c677905cc5cc3.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
